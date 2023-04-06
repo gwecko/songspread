dc8f4a22f1a8c0d9f5adefb6d1c8d739fc23408e
Grant Wecker <grantwecker@live.com>
2023-04-06 00:14:41 -0400

    UI improvements

---
+++ src/components/SignOutButton.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { signOut } from "next-auth/react";
+import { Box, Button } from "@chakra-ui/react";
+
+interface Props {}
+
+const SignOutButton: React.FC<Props> = () => {
+  return (
+    <Box p={8}>
+      <Button
+        colorScheme={"purple"}
+        _active={{ transform: "scale(0.95)" }}
+        onClick={() => signOut()}
+      >
+        Logout
+      </Button>
+    </Box>
+  );
+};
+
+export default SignOutButton;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,10 @@
 /* eslint-disable @next/next/no-img-element */
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '@/styles/Home.module.css'
-import { useSession } from "next-auth/react"
-import { SignInButton, TrackList } from '@/components'
-import { Box, Center, Flex, Heading, Spacer } from '@chakra-ui/react'
-
+import Head from "next/head";
+import Image from "next/image";
+import styles from "@/styles/Home.module.css";
+import { useSession } from "next-auth/react";
+import { SignInButton, TrackList, ListTabs } from "@/components";
+import { Box, Center, Flex, Heading, Spacer, Stack } from "@chakra-ui/react";
 
 // the home page; at location '/'
 export default function Home() {
@@ -16,18 +15,33 @@ export default function Home() {
     <>
       <Head>
         <title>Grant is good</title>
-        <meta name="description" content="Grant is good and this is his spotify app" />
+        <meta
+          name="description"
+          content="Grant is good and this is his spotify app"
+        />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
       {console.log(loading)}
-      <Box display={'flex'} flexDirection={'column'} minH={'100vh'} alignItems={'center'} bg={'gray.300'} bgGradient={'linear(to bottom, gray.300 60%, purple.200)'} bgAttachment={'fixed'}>
-        <Heading bgGradient={'linear(to bottom, gray.400 20%, purple.500)'} bgClip={'text'} fontSize={'5xl'} fontWeight={'extrabold'} paddingY={10}>SongSpread</Heading>
-          {
-            !session
-            ? <SignInButton isLoading={loading} />
-            : <TrackList session={ session } />
-          }
+      <Box
+        display={"flex"}
+        flexDirection={"column"}
+        minH={"100vh"}
+        alignItems={"center"}
+        bg={"gray.400"}
+        bgGradient={"linear(to bottom, gray.300 60%, purple.200)"}
+        bgAttachment={"fixed"}
+      >
+        <Heading
+          bgGradient={"linear(to bottom, gray.400 20%, purple.500)"}
+          bgClip={"text"}
+          fontSize={"5xl"}
+          fontWeight={"extrabold"}
+          paddingY={10}
+        >
+          SongSpread
+        </Heading>
+        {!session ? <SignInButton /> : <ListTabs session={session} />}
       </Box>
     </>
   );
