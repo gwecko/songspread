c63bddd6e4e192fd305f3a316c2546a1045890e1
Grant Wecker <grantwecker@live.com>
2023-04-11 15:05:15 -0400

    rough dom to image download

--- src/components/ListTabs.tsx
+++ src/components/ListTabs.tsx
@@ -1,11 +1,23 @@
 import { Tabs, TabList, Tab, TabPanels, TabPanel, Text, Divider } from "@chakra-ui/react";
 import TrackList from "./TrackList";
+import React from "react";
 
 interface Props {
   session: any
 }
 
 const ListTabs: React.FC<Props> = (Props) => {
+  
+  const timeRanges = {
+    short: 'short_term',
+    medium: 'medium_term',
+    long: 'long_term'
+  }
+  const { short, medium, long } = timeRanges
+  
+  // implement adjustable song numbers
+  // option to export playlist to account
+  
   
   return (
     <Tabs variant={"soft-rounded"} colorScheme={"purple"} align={"center"}>
@@ -19,13 +31,13 @@ const ListTabs: React.FC<Props> = (Props) => {
       
       <TabPanels>
         <TabPanel>
-          <TrackList session={Props.session} timeRange="short_term" />
+          <TrackList session={Props.session} timeRange={short} />
         </TabPanel>
         <TabPanel>
-          <TrackList session={Props.session} timeRange="medium_term" />
+          <TrackList session={Props.session} timeRange={medium} />
         </TabPanel>
         <TabPanel>
-          <TrackList session={Props.session} timeRange="long_term" />
+          <TrackList session={Props.session} timeRange={long} />
         </TabPanel>
       </TabPanels>
     </Tabs>
