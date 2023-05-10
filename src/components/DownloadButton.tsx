ae86f5da3570c9c20d590b9d8b02003200089fbd
Grant Wecker <grantwecker@live.com>
2023-05-10 13:18:54 -0400

    Opens list in new tab

--- src/components/DownloadButton.tsx
+++ src/components/DownloadButton.tsx
@@ -1,7 +1,7 @@
 import { Button } from "@chakra-ui/react";
 import html2canvas from "html2canvas";
 import Link from "next/link";
-import path from "path";
+import { Router, useRouter } from "next/router";
 import { useState } from "react";
 
 // supported CSS properties:
@@ -15,17 +15,23 @@ const imageOptions = {
 
 
 const DownloadButton: React.FC = () => {
+  
+  const router = useRouter()
 
-  const [imageUrl, setImageUrl] = useState('')
-  const getDataUrl = async () => {
+  const getDataUrl = async (e: any) => {
+    e.preventDefault()
     const element = document.getElementById("tabDownload")!,
-    canvas =  await html2canvas(element, imageOptions)
-    setImageUrl(canvas.toDataURL())
+      canvas = await html2canvas(element, imageOptions),
+      dataUrl = canvas.toDataURL()
+    router.push({
+      pathname: '/download/[dataUrl]',
+      query: { dataUrl: dataUrl },
+    })
   }
 
   return (
-    <Button mt={5} colorScheme={"purple"} onClick={getDataUrl}>
-      <Link href={`/download?dataUrl=${encodeURIComponent(imageUrl)}`}>Download Image</Link>
+    <Button mt={5} colorScheme={"purple"} onClick={(e)=>getDataUrl(e)} role='link'>
+        Download Image
     </Button>
   );
 };
--- src/pages/download/[dataUrl].tsx
+++ src/pages/download/[dataUrl].tsx
@@ -5,14 +5,14 @@ import React from "react";
 
 
 const DownloadPage: React.FC = () => {
-  const router = useRouter()
-  const dataUrl: string = router.query.dataUrl!
-  const imageUrl = decodeURIComponent(dataUrl)
   
+  const router = useRouter()
+  const imageUrl: any = router.query.dataUrl!
+  console.log(imageUrl)
   return (
     <Layout>
       <h1>booyah bitch</h1>
-      <Image src={imageUrl} alt="list of top songs" />
+      <Image src={imageUrl} id="song-image" alt="list of top songs" fill/>
       <h2>Press and hold to save</h2>
     </Layout>
   )
