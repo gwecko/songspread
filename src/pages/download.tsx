20a281ca1cdbfd21db3aa8602ceba50d6a27fbaa
Grant Wecker <grantwecker@live.com>
2023-05-09 23:57:11 -0400

    pushin lol

--- src/pages/download.tsx
+++ src/pages/download.tsx
@@ -1,9 +1,21 @@
 import { Layout } from "@/components";
+import Image from "next/image";
+import { useRouter } from "next/router";
+import React from "react";
 
-export default function DownloadPage() {
+
+const DownloadPage: React.FC = () => {
+  const router = useRouter()
+  const dataUrl: string = router.query.dataUrl!
+  const imageUrl = decodeURIComponent(dataUrl)
+  
   return (
     <Layout>
       <h1>booyah bitch</h1>
+      <Image src={imageUrl} alt="list of top songs" />
+      <h2>Press and hold to save</h2>
     </Layout>
   )
 }
+
+export default DownloadPage
