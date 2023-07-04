e9333c150d9210a3d64792f64b76db5b8a1b3806
Grant Wecker <grantwecker@live.com>
2023-07-04 15:06:50 -0400

    minor refactoring

--- src/components/LoadingSkeleton.tsx
+++ src/components/LoadingSkeleton.tsx
@@ -1,19 +1,22 @@
 import { Stack, Skeleton } from "@chakra-ui/react";
 
 interface Props {
-  length: number
+  maxLength: number
+  displayLength: number
 }
 
 /**
+ * Parameters are distinct in order to prevent re-render of array on each change:
  * 
- * @param length individual skeleton blocks to create
+ * @param maxLength number of skeleton blocks to generate
+ * @param displayLength individual skeleton blocks to render
  * @see https://chakra-ui.com/docs/components/skeleton/props
  * @returns chakra UI Skeleton JSX
  */
-const LoadingSkeleton: React.FC<Props> = ({length}) => {
+const LoadingSkeleton: React.FC<Props> = ({displayLength, maxLength}) => {
   
   /* timing oscillates based on index */
-  const interval = Array.from({ length: length }).map((_, i) => {
+  const interval = Array.from({ length: maxLength }).map((_, i) => {
     return i % 4 ? (i % 2 ? 0.7 : 1.1) : 0.6;
   });
   
@@ -25,8 +28,8 @@ const LoadingSkeleton: React.FC<Props> = ({length}) => {
 
   return (
     <Stack w={"65vw"} maxW={"600px"}>
-      {Array.from({ length: length }).map((_, i) =>
-        i < length ? (
+      {Array.from({ length: maxLength }).map((_, i) =>
+        i < displayLength ? (
           <Skeleton {...skeletonStyles} key={i} speed={interval[i]} />
         ) : null
       )}
