4db835390bb50808f240eac400eb25d101c689cb
Grant Wecker <grantwecker@live.com>
2023-07-11 16:53:22 -0400

    border animation changes

--- src/components/BorderAnimation.tsx
+++ src/components/BorderAnimation.tsx
@@ -4,14 +4,17 @@ import { ReactNode } from "react";
 
 interface Props {
   children: ReactNode;
+  width?: number;
+  height?: number;
 }
 
-const BorderAnimation: React.FC<Props> = ({ children }) => {
+const BorderAnimation: React.FC<Props> = ({ children, width, height }) => {
   const colorStart = "#CBD5E0";
   const colorEnd = "#B794F4";
 
   return (
     <Box m={'auto'} w={'fit-content'}>
+      {/* the overlay that creates a pseudo-border*/}
       <motion.div
         style={{
           position: "relative",
@@ -23,13 +26,14 @@ const BorderAnimation: React.FC<Props> = ({ children }) => {
           width: '100%'
         }}
       >
+        {/* rotating gradient div for illusing of moving border */}
       <motion.div
         style={{
           position: "absolute",
           background: `conic-gradient(from 0deg, ${colorStart}, ${colorEnd}, ${colorStart})`,
-          height: '500px',
-          width: '500px',
-          top: '50%',
+          height: height ? 2 * height : '80vw',
+          width: width ? 2 * width : '80vw',
+          top: '40%',
           translateY: '-50%',
           left: '50%',
           translateX: '-50%',
@@ -38,7 +42,7 @@ const BorderAnimation: React.FC<Props> = ({ children }) => {
         animate={{
           rotate: 360,
         }}
-        transition={{ duration: 2, repeat: Infinity, ease: "linear" }}
+        transition={{ duration: 4, repeat: Infinity, ease: "linear" }}
       />
         {children}
       </motion.div>
