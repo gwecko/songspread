947d3e001eb607b42e7c8441774cc432f17034eb
Grant Wecker <grantwecker@live.com>
2023-07-20 10:58:32 -0400

    skeleton tweak

--- src/components/ListTabs.tsx
+++ src/components/ListTabs.tsx
@@ -24,7 +24,7 @@ const ListTabs: React.FC<Props> = ({ session }) => {
   const [numTracksToDisplay, setNumTracksToDisplay] = useState(5);
   const panelStyles = {
     m: 0,
-    pl: '30px',
+    pl: '15px',
   }
 
   return (
